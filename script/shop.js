8e275a2b9f4e3f58a7cb581f4e76029304cc27bf
Nicolás García <nicolasg.ngl@gmail.com>
2022-01-16 22:28:24 -0500

    Agregando pagina individual de crown.html con su respectivo CSS y JS

--- script/shop.js
+++ script/shop.js
@@ -1,16 +1,15 @@
 
-export const datos = 'http://localhost:3000/Products';
+export const datos = 'http://localhost:3000/Products/';
 
 const cards = document.getElementById('cont');
 
 const getData = async () =>{
-    console.log(cards);
+    
     const res = await fetch(datos);
     const data = await res.json();
     
         data.forEach(element => {
-            const {id, nombre, imagen, imagen2, imagen3, precio} = element;
-       
+            const {id, nombre, imagen, imagen2, imagen3, precio} = element
         cards.innerHTML += `
         <div id="card" style="margin: auto; width: 30%">
             <img class="d-block (display-block) mx-auto (margin-auto) " style="width: 60% " src="${imagen}" title="${nombre}" alt="">
